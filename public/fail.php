6558b9adbfc8b1591b326f1be9a0fda3f458fcbe
Michelangelo <dragonbe+github@gmail.com>
2011-04-13 10:39:21 +0200

    explicit writing failure for PHP -l demo

---
+++ public/fail.php
@@ -0,0 +1,10 @@
+<?php
+// file: public/fail.php
+
+// a missing closing ; will trigger a PHP Parse error!
+//$var = 'ABC'
+
+// this will trigger a PHP Fatal error!
+//function a() {}
+//
+//function a() {}
